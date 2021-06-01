380632f95556e14880efd334c2b011c489ec2cd3
tkulic <toni.kulic@yahoo.com>
2021-06-01 18:58:32 +0200

    Desktop navigation fixed

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,10 +2,6 @@ import React from 'react'
 import FooterNav from './FooterNav'
 import SocialMedia from './SocialMedia'
 
-import facebook from "../../images/icon-facebook.svg"
-import twitter from "../../images/icon-twitter.svg"
-import pinterest from "../../images/icon-pinterest.svg"
-import instagram from "../../images/icon-instagram.svg"
 import Logo from '../../images/Logo'
 
 export default function Footer() {
@@ -24,7 +20,7 @@ export default function Footer() {
                 footerNavTitle="Company"
                 footerNavLinks={["About", "Our Team", "Careers", "Contact"]}
             />
-            <SocialMedia socialMediaItems={[facebook, twitter, pinterest, instagram]} />
+            <SocialMedia />
             <div className="attribution">
                 <p>Challenge by <a className="attribution-link" href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a></p>
                 <p>Coded by <a href="https://github.com/tkulic" target="_blank" rel="noreferrer">Toni Kulic</a></p>
